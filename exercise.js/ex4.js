3079f89db6173b323439fe5b58a237dcde11637a
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-17 21:03:02 -0700

    ex4.js

---
+++ exercise.js/ex4.js
@@ -0,0 +1,14 @@
+let  num1 = 30
+let num2 = 40
+let num3 = 60
+
+if (num1 + num2 > num3) {
+    if (num1 + num3 > num2) {
+        if (num2 + num3 > num1) {
+            console.log("possible")
+        } 
+    }
+} else { 
+    console.log("impossible")
+
+}
