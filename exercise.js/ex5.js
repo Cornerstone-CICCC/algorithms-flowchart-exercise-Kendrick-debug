4c7deb10538259cb020429e4223a252983e83816
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-17 21:22:03 -0700

    ex5

---
+++ exercise.js/ex5.js
@@ -0,0 +1,13 @@
+let student = "Daz"
+const fullScore = 100
+let mark = 20
+ 
+if (mark >= fullScore * 0.8) {
+    console.log("A")
+} else if (mark >= fullScore * 0.6) {
+    console.log("B")
+} else if (mark >= fullScore * 0.4) {
+    console.log("C")
+} else {
+    console.log("no grade")
+}
