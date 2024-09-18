fe0fb60cf534957cd781844895bc74325cc269e5
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-17 20:47:20 -0700

    ex3.js

---
+++ exercise.js/ex3.js
@@ -0,0 +1,13 @@
+let num1 = 30
+let num2 = 40
+let num3 = 50
+if (num1 > num2) {
+    if (num1 > num3) {
+        console.log("num1 has the highest value")
+    } 
+} else if (num2 > num3) {
+    console.log("num2 has the highest value")
+} else {
+    console.log("num3 has the hight value")
+}
+
